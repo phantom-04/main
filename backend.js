44865fa1953741dd338db41d2fecfaa32045bb37
Ivan Božić <65293315+phantom-04@users.noreply.github.com>
2023-08-08 09:22:55 +0200

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,14 +1,1 @@
-const http = require('http');
-
-
-const server = http.createServer(route);
-const {Builder, By, Key, until} = require('selenium-webdriver');
-let driver;
-
-
-server.listen(3000);
-
 res.write("<h1>Hallo<\h1>");
-
-    res.end();
-
