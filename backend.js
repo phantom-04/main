3ce813403c74f683c305502a5491bf59e785b856
Ivan Božić <65293315+phantom-04@users.noreply.github.com>
2023-08-08 09:19:05 +0200

    Update backend.js

--- backend.js
+++ backend.js
@@ -8,23 +8,7 @@ let driver;
 
 server.listen(3000);
 
-
-
-function route(req, res) {
-
-    if(req.url == '/') {
-        res.write('<h1>Ivan ist krass</h1>');
-    }
-    else if(req.url == '/test1') {
-        res.write('<h1>test1</h1>');
-    }
-    else if(req.url == '/test2') {
-        res.write('<h1>test2</h1>');
-    }
-    else {
-        //res.write('<h1>404</h1>');
-        res.write('<h1>' + req.url.slice(1) + '</h1>')
-    }
+res.write("<h1>Hallo<\h1>");
 
     res.end();
 }
